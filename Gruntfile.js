15c870aecc1b280bee7179fca5ed2c90cc2b391b
asciidisco <public@asciidisco.com>
2014-03-04 17:31:48 +0100

    12-wraith

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,23 @@ module.exports = function (grunt) {
       }
     },
 
+    // patternprimer config
+    patternprimer: {
+      options: {
+        wwwroot: 'patterns',
+        css: ['http://localhost:5001/styles/style.css'],
+        dest: 'docs'
+      },
+      live: {
+        ports: [7020, 7030],
+        src: 'patterns'
+      },
+      snapshot: {
+        snapshot: true,
+        src: 'patterns'
+      }
+    },
+
     // dalekjs config
     dalek: {
       options: {
@@ -32,6 +49,12 @@ module.exports = function (grunt) {
 
     // execute shell commands for hardy & wraith
     shell: {
+      wraith: {
+        options: {
+          stdout: true
+        },
+        command: 'wraith capture config'
+      },
       setupHardy: {
         options: {
           stdout: true
@@ -49,8 +72,8 @@ module.exports = function (grunt) {
           stdout: true
         },
         command: 'hardy selenium stop'       
-      }                     
-    } 
+      },                         
+    }
 
   });
 
@@ -59,6 +82,7 @@ module.exports = function (grunt) {
 
   // load external grunt tasks
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-patternprimer');
   grunt.loadNpmTasks('grunt-dalek');
   grunt.loadNpmTasks('grunt-shell');  
 };
---
+++ patterns/heading-2.html
@@ -0,0 +1,1 @@
+<h2>Headline 2</h2>
