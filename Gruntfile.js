02110fa1b472b84575af294f5ee20ffa061df81f
asciidisco <public@asciidisco.com>
2014-03-04 16:40:01 +0100

    10-grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,27 @@ module.exports = function (grunt) {
           base: 'public'
         }
       }
-    }
+    },
+
+    // dalekjs config
+    dalek: {
+      options: {
+        reporter: ['html'],
+        dalekfile: false
+      },
+      phantom: {
+        src: ['test/dalek/*.js']
+      },
+      chrome: {
+        options: {
+          browser: ['chrome']
+        },
+        src: ['test/dalek/*.js']    
+      }
+    }    
   });
 
   // load external grunt tasks
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-dalek');
 };
